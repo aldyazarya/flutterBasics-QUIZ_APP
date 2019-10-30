59a6a7233c29a190390ff910afcc1957e25d03f7
aldyazarya <aldy1612@gmail.com>
2019-10-30 22:45:27 +0700

    mapping list for question and fill value of answer in button widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
+  final String answer;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answer);
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +12,8 @@ class Answer extends StatelessWidget {
       width: double.infinity,
       child: RaisedButton(
         color: Colors.blue,
-        child: Text('answer 1'),
+        textColor: Colors.white,
+        child: Text(answer),
         onPressed: selectHandler, //declare function
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import './answer.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -18,7 +17,7 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State<MyApp> { 
+class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   // create function to onPressed
@@ -31,10 +30,19 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-
     var questions = [
-      'What\s your favourite color?',
-      'What\s your favourite animal?',
+      {
+        'questionText': 'What\s your favourite color?',
+        'answer': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\s your favourite animal?',
+        'answer': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      },
+      {
+        'questionText': 'Who\s your favourite member black pink?',
+        'answer': ['Jisoo', 'Jenny', 'Lisa', 'Rose']
+      }
     ];
     return MaterialApp(
       home: Scaffold(
@@ -44,12 +52,11 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             // Text(questions.elementAt(0)), // function elementAt for indexing just like questions[0]
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            
-          ],
+            Question(questions[_questionIndex]['questionText']),
+            ...(questions[_questionIndex]['answer'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ]
         ),
       ),
     );
